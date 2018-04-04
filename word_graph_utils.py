8a0bef089995f59b9e6e45910b3b71eef437c22b
panyan <panyan@tmxmall.com>
2018-04-04 20:51:53 +0800

    init

--- word_graph_utils.py
+++ word_graph_utils.py
@@ -16,7 +16,7 @@ def word_freq(k, ks):
 
 with open("./text/lans_en.txt", "r", encoding="utf8") as f:
     text = f.readlines()
-text = text * 100
+text = text * 200
 text = [[x.strip(), word_freq(k, len(text))] for k, x in enumerate(text)]
 
 
@@ -33,15 +33,15 @@ aaa = aa(text)
 
 mask_pic = np.array(Image.open(mask_path))
 
-mask_pic_2 = cv2.resize(mask_pic, (mask_pic.shape[1] * 3, mask_pic.shape[0] * 3))
+mask_pic_2 = cv2.resize(mask_pic, (mask_pic.shape[1] * 5, mask_pic.shape[0] * 5))
 
 wc = WordCloud(font_path=font_path,
                prefer_horizontal=1,
                background_color="white",
                mask=mask_pic_2,
                max_font_size=300, random_state=80, margin=0,
-               max_words=2000,
-               width=2000, height=2000
+               max_words=5000,
+               width=3000, height=3000
                )
 # generate word cloud
 wc.generate_from_frequencies(aaa)
